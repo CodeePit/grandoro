b82a4b398c837bd042f3ade17d7718b56bc3e4e6
AfonsoBezerra <afonsobezerra199@gmail.com>
2026-08-24 13:49:45 -0300

    feat: integrate Facebook Pixel for enhanced tracking and analytics
    - Added Facebook Pixel script to the layout for tracking page views.
    - Included noscript fallback for users with JavaScript disabled.
    - Defined FB_PIXEL_ID constant for easy configuration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Montserrat } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -24,6 +25,7 @@ const condor = localFont({
 });
 
 const BASE_URL = "https://grandoro.com.br";
+const FB_PIXEL_ID = "1094457696248084";
 
 export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
@@ -89,7 +91,33 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
       </head>
-      <body className="min-h-full flex flex-col antialiased">{children}</body>
+      <body className="min-h-full flex flex-col antialiased">
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${FB_PIXEL_ID}');
+fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+        {children}
+      </body>
     </html>
   );
 }
